c923ddd5e9f541985bbff66930af0d767a972354
nvkhanh123 <nguyenvankhanhxm123@gmail.com>
2020-09-10 19:50:07 +0700

    update sql into

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ router.get('/hash', function (req, res) {
     var lastId = parseInt(gameId.toString());
     var array = [];
     var firstObject = { hash: hash, index: lastId, crash: lib.crashPointFromBustabit(hash), query: ""}; 
-    firstObject.query = 'INSERT INTO BustabitGameInfo (GameId, Hash, CrashPoint) VALUES ( '+ firstObject.index + ', '+ firstObject.hash +', ' + firstObject.crash + ');';
+    firstObject.query = 'INSERT INTO BustabitGameInfo (GameId, Hash, CrashPoint) VALUES ( '+ firstObject.index + ', \''+ firstObject.hash +'\', ' + firstObject.crash + ');';
 
     array.push(firstObject);
-    for (let index = 2; index <= 2000; index++) {
+    for (let index = 2; index <= 5000; index++) {
         lastId--;
         hash = lib.genGameHash(hash);
         var tempObject =  { hash: hash, index: lastId, crash: lib.crashPointFromBustabit(hash), query: ""};
-        tempObject.query = 'INSERT INTO BustabitGameInfo (GameId, Hash, CrashPoint) VALUES ( '+ tempObject.index + ', '+ tempObject.hash +', ' + tempObject.crash + ');';
+        tempObject.query = 'INSERT INTO BustabitGameInfo (GameId, Hash, CrashPoint) VALUES ( '+ tempObject.index + ', \''+ tempObject.hash +'\', ' + tempObject.crash + ');';
         array.push(tempObject);
     }
     res.send(JSON.stringify(array));
